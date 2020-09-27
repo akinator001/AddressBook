a92b548d4718157f955ee7929a8b84f8fab50a82
akinator001 <aakashgandhi1279@gmail.com>
2020-09-27 21:13:33 +0530

    [aakash] Added , Created a Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,14 @@
 package com.capgemini.addressbook;
 
+import java.util.*;
 public class AddressBookMain {
 	public static void main(String []args) {
+		
 		System.out.println("welcome to Address Book \r\n"+ 
 							"Program \r\n"+
 							 "AddressBookMain class on Master Branch" );
+		ArrayList<AddressBookContent> list = new ArrayList<>();
+		list.add(new AddressBookContent("Aakash","Gandhi","Model Town","Panipat",132103,"8708155286","aakash.g@gmail.com" ));
+		System.out.println("Contact created !!!");
 	}
 }
