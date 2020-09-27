baa2ef37f999c44d05ccb7d687c654ffc7dd9e12
akinator001 <aakashgandhi1279@gmail.com>
2020-09-27 22:02:15 +0530

    [aakash]Added uc2: Ability to Add New Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,11 +4,33 @@ import java.util.*;
 public class AddressBookMain {
 	public static void main(String []args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("welcome to Address Book \r\n"+ 
 							"Program \r\n"+
 							 "AddressBookMain class on Master Branch" );
 		ArrayList<AddressBookContent> list = new ArrayList<>();
+		
+		System.out.println("Enter first name");
+		String fname = sc.next();
+		System.out.println("Enter last name");
+		String lname = sc.next();
+		System.out.println("Enter Address");
+		String add = sc.nextLine();
+		sc.nextLine();
+		System.out.println("Enter City");
+		String city = sc.next();
+		sc.nextLine();
+		System.out.println("Enter zip code");
+		int zipcode = sc.nextInt();
+		System.out.println("Enter Phone Number");
+		String ph = sc.next();
+		System.out.println("Enter E-Mail");
+		String mail = sc.next();
+
 		list.add(new AddressBookContent("Aakash","Gandhi","Model Town","Panipat",132103,"8708155286","aakash.g@gmail.com" ));
+		list.add(new AddressBookContent(fname, lname, add, city, zipcode, ph, mail));
+
 		System.out.println("Contact created !!!");
 	}
 }
