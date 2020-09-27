00afdb281a37ed50eab3b682e2bdd5d424165f08
akinator001 <aakashgandhi1279@gmail.com>
2020-09-27 18:12:26 +0530

    [aakash] initial commit ,Display Welcome Message

---
+++ AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.capgemini.addressbook;
+
+public class AddressBookMain {
+	public static void main(String []args) {
+		System.out.println("welcome to Address Book \r\n"+ 
+							"Program \r\n"+
+							 "AddressBookMain class on Master Branch" );
+	}
+}
